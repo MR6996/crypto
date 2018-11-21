bc42e308c1007424d349eebc34bdfd4a8dc044ce
Mario Randazzo <Mario Randazzo@DESKTOP-B55GO8M.lan>
2018-11-21 23:33:34 +0100

    discreteLog implemented

--- src/main/java/cryptography/Utils.java
+++ src/main/java/cryptography/Utils.java
@@ -1,19 +1,26 @@
 package cryptography;
 
-import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public class Utils {
 
-	public static BigDecimal inverseMod(BigDecimal n, BigDecimal m) {
-		//TODO implement inverseMod
-		return null;
+	public static <T extends Groupable<T>> BigInteger discreteLog(T n, T g, BigInteger m, long attempt) {
+		//TODO write unit test for discreteLog
+		if (n.equals(g))
+			return new BigInteger("1");
+
+		T temp = g.add(g, m);
+		long i = 2;
+		while (i < attempt && n.equals(temp)) {
+			temp = temp.add(g, m);
+			i++;
+		}
+
+		return new BigInteger(Long.toString(i));
 	}
-	
-	public static BigDecimal discreteLog(BigDecimal n, BigDecimal g, BigDecimal m) {
-		//TODO implement discreteLog
-		return null;
+
+	public static <T extends Groupable<T>> BigInteger discreteLog(T n, T g, BigInteger m) {
+		return discreteLog(n, g, m, 100000);
 	}
-	
-	
-	
+
 }
