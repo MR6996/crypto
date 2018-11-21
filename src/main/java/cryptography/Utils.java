66af4a75c844432603995c16c253c0edea781af6
Mario Randazzo <Mario Randazzo@DESKTOP-B55GO8M.lan>
2018-11-21 19:48:07 +0100

    Create class for utils methods

---
+++ src/main/java/cryptography/Utils.java
@@ -0,0 +1,19 @@
+package cryptography;
+
+import java.math.BigDecimal;
+
+public class Utils {
+
+	public static BigDecimal inverseMod(BigDecimal n, BigDecimal m) {
+		//TODO implement inverseMod
+		return null;
+	}
+	
+	public static BigDecimal discreteLog(BigDecimal n, BigDecimal g, BigDecimal m) {
+		//TODO implement discreteLog
+		return null;
+	}
+	
+	
+	
+}
