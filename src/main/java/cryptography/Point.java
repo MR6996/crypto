40cd1855e568ddc33c8eb51d6e38e82640035ab5
Mario Randazzo <randazzo.mario6996@gmail.com>
2018-11-24 17:38:04 +0100

    Add z coordinate, implemented inverse, add, and multiply

--- src/main/java/cryptography/Point.java
+++ src/main/java/cryptography/Point.java
@@ -6,12 +6,20 @@ import java.math.BigInteger;
 
 public class Point implements Groupable<Point> {
 
+	public static final Point OMEGA = new Point(BigInteger.ZERO, BigInteger.ONE, BigInteger.ZERO);
+
 	BigInteger x;
 	BigInteger y;
+	BigInteger z;
 
 	public Point(BigInteger x, BigInteger y) {
+		this(x, y, BigInteger.ONE);
+	}
+
+	public Point(BigInteger x, BigInteger y, BigInteger z) {
 		this.x = x;
 		this.y = y;
+		this.z = z;
 	}
 
 	public BigInteger getX() {
@@ -30,23 +38,100 @@ public class Point implements Groupable<Point> {
 		this.y = y;
 	}
 
+	public BigInteger getZ() {
+		return z;
+	}
+
 	public Point getElement() {
 		return this;
 	}
 
 	public Point add(Point other, BigInteger module) {
-		// TODO implement sum
-		return null;
+		if (this.z.equals(BigInteger.ZERO))
+			return other;
+
+		if (other.z.equals(BigInteger.ZERO))
+			return this;
+
+		//@formatter:off
+		if (this.x.equals(other.x)) {
+			if (this.y.add(other.y).equals(BigInteger.ZERO))
+				return OMEGA;
+			else {
+				//inv = inverse((2 * a.y)%n, n)
+				//x2p = ( ((3*a.x**2)*inv)**2 - 2*a.x ) %n
+				//return Point( x2p , (-a.y -(3*a.x**2)*inv*(x2p - a.x)) %n )
+				BigInteger x2pPartial =  this.x
+											.pow(2)
+											.multiply(new BigInteger("3"))
+											.multiply(
+												this.y
+													.add(this.y)
+													.modInverse(module)
+											)
+											.mod(module);
+				BigInteger x2p = x2pPartial
+									.pow(2)
+									.subtract(this.x)
+									.subtract(this.x)
+									.mod(module);
+				return new Point(
+					x2p,
+					this.y.negate().subtract(x2pPartial.multiply(x2p.subtract(this.x))).mod(module)
+				);
+			}
+		} else {
+			// m = ((a.y - b.y)*inverse((a.x - b.x)%n, n)) %n
+			BigInteger m = this.y.subtract(other.y).multiply(this.x.subtract(other.x).modInverse(module)).mod(module);
+			// (m**2 - a.x - b.x) %n, (-a.y-m*(m**2 - 2*a.x - b.x)) %n
+			return new Point(
+					m.multiply(m)
+						.subtract(this.x)
+						.subtract(other.x)
+						.mod(module), 
+					this.y
+						.negate()
+						.subtract(
+							m.multiply(m)
+								.subtract(this.x)
+								.subtract(this.x)
+								.subtract(other.x)
+								.multiply(m)
+						)
+						.mod(module)
+			);
+			
+		}
+		// @formatter:on
+
+		// TODO implement sum ☺
 	}
 
 	public Point multiply(BigInteger n, BigInteger module) {
-		// TODO implement multiply
-		return null;
+		Point result = OMEGA;
+		Point partial = this;
+		int j = 0;
+		
+		if (n.testBit(0)) {
+			result = result.add(partial, module);
+			j++;
+		}
+		
+		for (int i = 1; i < n.bitLength() && j < n.bitCount(); i++) {
+			partial = partial.add(partial, module);
+			if (n.testBit(i)) {
+				result = result.add(partial, module);
+				j++;
+			}
+		}
+
+		// TODO implement multiply ☺
+		return result;
 	}
 
 	public Point getInverse(BigInteger module) {
-		// TODO implement inverse
-		return null;
+		// TODO implement inverse ☺
+		return new Point(x, y.negate());
 	}
 
 	@Override
@@ -82,9 +167,7 @@ public class Point implements Groupable<Point> {
 
 	@Override
 	public String toString() {
-		return "[" + x + "; " + y + "]";
+		return "[" + x + "; " + y + "; " + z + "]";
 	}
-	
-	
-	
+
 }
