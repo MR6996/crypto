ed9481c5299b0633955d20a1afda420a2e19b34d
Mario Randazzo <randazzo.mario6996@gmail.com>
2018-11-24 17:39:32 +0100

    Add methods for factorization and bezout identity, add RSA class.

---
+++ src/main/java/cryptography/RSAHacker.java
@@ -0,0 +1,39 @@
+package cryptography;
+
+import java.math.BigInteger;
+
+public class RSAHacker {
+
+	private BigInteger n;
+	private BigInteger e;
+	private BigInteger d;
+
+	public RSAHacker(BigInteger n, BigInteger e) {
+		this.n = n;
+		this.e = e;
+
+		BigInteger p_1 = Utils.rhoFactorization(n);
+		BigInteger q_1 = n.divide(p_1).subtract(BigInteger.ONE);
+		p_1 = p_1.subtract(BigInteger.ONE);
+		BigInteger lamda = p_1.multiply(q_1).divide(p_1.gcd(q_1));
+
+		this.d = Utils.Bezout(lamda, e)[0];
+	}
+
+	public BigInteger getN() {
+		return n;
+	}
+
+	public BigInteger getPublicKey() {
+		return e;
+	}
+
+	public BigInteger getPrivateKey() {
+		return d;
+	}
+
+	public BigInteger decrypt(BigInteger c) {
+		return c.modPow(d, n);
+	}
+
+}
--- src/main/java/cryptography/Utils.java
+++ src/main/java/cryptography/Utils.java
@@ -2,10 +2,13 @@ package cryptography;
 
 import java.math.BigInteger;
 
+//TODO Write documentation
+
 public class Utils {
 
+	private static final int CERTAINTY = 50;
+
 	public static <T extends Groupable<T>> BigInteger discreteLog(T n, T g, BigInteger m, long attempt) {
-		//TODO write unit test for discreteLog
 		if (n.equals(g))
 			return new BigInteger("1");
 
@@ -21,6 +24,60 @@ public class Utils {
 
 	public static <T extends Groupable<T>> BigInteger discreteLog(T n, T g, BigInteger m) {
 		return discreteLog(n, g, m, 100000);
+	}
+
+	public static BigInteger rhoFactorization(BigInteger n) {
+		if (n.isProbablePrime(CERTAINTY))
+			throw new ArithmeticException("n is probably prime!");
+
+		BigInteger xi = f(new BigInteger("2"), n);
+		BigInteger x2i = f(xi, n);
+		BigInteger gcd;
+
+		do {
+			if (!(gcd = x2i.subtract(xi).gcd(n)).equals(BigInteger.ONE))
+				if (gcd.isProbablePrime(CERTAINTY))
+					return gcd;
+				else
+					return rhoFactorization(gcd);
+
+			xi = f(xi, n);
+			x2i = f(f(x2i, n), n);
+
+		} while (true);
+
+	}
+
+	private static BigInteger f(BigInteger x, BigInteger module) {
+		return x.multiply(x).add(BigInteger.ONE).mod(module);
+	}
+
+	public static BigInteger[] Bezout(BigInteger a, BigInteger b) {
+		BigInteger tmp;
+		if (a.compareTo(b) > 0) {
+			tmp = a;
+			a = b;
+			b = tmp;
+		}
+		
+		BigInteger s0 = BigInteger.ONE, s1 = BigInteger.ZERO;
+		BigInteger t0 = BigInteger.ZERO, t1 = BigInteger.ONE;
+		
+		BigInteger[] r = b.divideAndRemainder(a);
+		while (r[1].compareTo(BigInteger.ZERO) > 0) {
+			tmp = s1;
+			s1 = s0.subtract(r[0].multiply(s1));
+			s0 = tmp;
+			tmp = t1;
+			t1 = t0.subtract(r[0].multiply(t1));
+			t0 = tmp;
+
+			b = a;
+			a = r[1];
+			r = b.divideAndRemainder(a);
+		}
+
+			return new BigInteger[] {t1, s1} ;
 	}
 
 }
